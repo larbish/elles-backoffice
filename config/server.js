38aee882ff66264c9b92b58fe479cba58f780ff9
Baptiste Leproux <baptiste.leproux@neo9.fr>
2021-05-21 12:46:12 +0200

    init(app): First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d2e32ea892ff11fa842a27a79f997fb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6f6332d-6604-49c0-b61a-f3014a530a05'
+};
